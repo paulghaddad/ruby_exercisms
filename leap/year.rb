b4dc7aa1458bcb9911729a1d2ace8b8d3d6ce999
Paul Haddad <paulh16@gmail.com>
2015-03-30 08:49:55 -0500

    Iteration 3

--- leap/year.rb
+++ leap/year.rb
@@ -3,10 +3,10 @@ class Year
     return false if leap_year_exception?(year)
     year % 4 == 0
   end
-  
+
   private
 
-    def self.leap_year_exception?(year)
-      year % 100 == 0 && year % 400 != 0
-    end
+  def self.leap_year_exception?(year)
+    year % 100 == 0 && year % 400 != 0
+  end
 end
