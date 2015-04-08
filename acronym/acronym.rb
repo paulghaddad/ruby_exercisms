4ae976b2db8e18210c487eb037b307583af598fb
Paul Haddad <paulh16@gmail.com>
2015-04-08 11:09:28 -0500

    Iteration 3

--- acronym/acronym.rb
+++ acronym/acronym.rb
@@ -1,21 +1,7 @@
 class Acronym
 
   def self.abbreviate(phrase)
-    case phrase
-    when /Portable Network Graphics/i
-      "PNG"
-    when /Ruby on Rails/i
-      "ROR"
-    when /HyperText Markup Language/i
-      "HTML"
-    when /First In, First Out/i
-      "FIFO"
-    when /PHP: Hypertext Preprocessor/i
-      "PHP"
-    when /Complementary metal-oxide semiconductor/i
-      "CMOS"
-    else
-      "There is no acronym for this phrase!"
-    end
+    acronym_letter = /[\s-]|[^A-Z](?=[A-Z])/
+    phrase.split(acronym_letter).inject("") { |acronym, word| acronym << word[0].upcase }
   end
 end
