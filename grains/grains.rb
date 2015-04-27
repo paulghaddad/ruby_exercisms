2bd736518ccef5e29eda84388f5e6f5ad4a1029f
Paul Haddad <paulh16@gmail.com>
2015-04-27 08:01:01 -0500

    Grain exercise - Iteration 2
    Refactor for magic number

--- grains/grains.rb
+++ grains/grains.rb
@@ -1,9 +1,11 @@
 class Grains
+  CHECKERBOARD_SQUARES = 64
+
   def square(square_number)
     2 ** (square_number - 1)
   end
 
-  def total(number_of_squares=64)
+  def total(number_of_squares = CHECKERBOARD_SQUARES)
     return 1 if number_of_squares == 1
     square(number_of_squares) + total(number_of_squares - 1)
   end
