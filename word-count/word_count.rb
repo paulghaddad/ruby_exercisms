63f2a207e9044612cf16a4f53620f719044568d4
Paul Haddad <paulh16@gmail.com>
2015-05-01 08:40:17 -0500

    Word Count Iteration 1
    Just get all the tests passing

---
+++ word-count/word_count.rb
@@ -0,0 +1,24 @@
+class Phrase
+  attr_reader :phrase
+
+  def initialize(phrase)
+    @phrase = sanitize(phrase)
+  end
+
+  def word_count
+    words = phrase.split(' ')
+    words.each_with_object({}) do |word, library|
+      if library[word]
+        library[word] += 1
+      else
+        library[word] = 1
+      end
+    end
+  end
+
+  private
+
+  def sanitize(phrase)
+    phrase.downcase.gsub(/,/, ' ').gsub(/[^\w\s']/, '').gsub(/\s\s/, ' ')
+  end
+end
